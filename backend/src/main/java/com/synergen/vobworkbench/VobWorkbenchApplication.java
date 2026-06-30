e6917c8c61789e64d09d7ec0639f403123620e47
Dilumin Silva <diluminshyamal@gmail.com>
2026-06-30 05:23:49 -0500

    initiated project

---
+++ backend/src/main/java/com/synergen/vobworkbench/VobWorkbenchApplication.java
@@ -0,0 +1,13 @@
+package com.synergen.vobworkbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VobWorkbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VobWorkbenchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/synergen/vobworkbench/VobWorkbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synergen.vobworkbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VobWorkbenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
